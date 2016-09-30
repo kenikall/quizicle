7199dc627dcc14ff2f07995ff3de78b699f8ae44
K. Mannah Kallon <kenikall@gmail.com>
2016-09-30 13:15:53 -0700

    MVP

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,18 +1,37 @@
-#login student
+
 get '/students' do
-  @user = Student.find_by_email(params[:email])
-  if @user && @user.password == params[:password]
-    session[:id]=@user.id
+  @student = Student.find_by_email(params[:email])
+  if @student && @student.password == params[:password]
+    session[:id]=@student.id
     session[:login_type]="student"
-    redirect "/students/#{@user.id}"
+    redirect "/students/#{@student.id}"
   else
-    erb :"students"
+    erb :"students/#{@student.id}"
   end
 end
-# Remember all those challenges on being restful?
+
+get '/students/:id' do
+  @student = Student.find(params[:id])
+  erb :"/students/show"
+  end
+
 post '/students/guest/:id' do
     @quiz = Quiz.find(params[:id])
     @quiz.get_response(params["response"].to_i)
 
     erb :"students/guest"
   end
+
+get '/students' do
+  erb :"/students/new"
+end
+
+post '/students' do
+  @student = Student.new(params)
+  if @student.save
+    session[:id] = @student.id
+    redirect "/students/#{@student.id}"
+  else
+    erb :"/students/new"
+  end
+end
--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,11 +1,11 @@
-# class Problem < ActiveRecord::Base
+class Problem < ActiveRecord::Base
   belongs_to :quiz
 
   def solving_time
-    # return difference between creation time and update time
+    self.updated_at-self.created_at
   end
 
   def check_answer
-    @correct_answer.to_s.downcase == @response.to_s.downcase
+    self.correct_answer.to_s.downcase == self.response.to_s.downcase
   end
 end
